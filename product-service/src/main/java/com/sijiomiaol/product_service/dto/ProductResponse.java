b62bab0bd69944f783c2df3af27d62ef83ec7faf
Sijibomiaol <aderinlewom@gamil.com>
2024-10-03 14:00:37 -0400

    Second Committ

--- product-service/src/main/java/com/sijiomiaol/product_service/dto/ProductResponse.java
+++ product-service/src/main/java/com/sijiomiaol/product_service/dto/ProductResponse.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @NoArgsConstructor
 @Component
 public class ProductResponse {
-    private UUID id;
     private String name;
     private String description;
     private BigDecimal price;
